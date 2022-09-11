7d1972f29d4eae94bfb85b92edd6c58650491a8d
Ronan Potts <ronanpotts6@gmail.com>
2022-09-11 13:10:31 +1000

    Updated app.r

--- app.R
+++ app.R
@@ -2,6 +2,15 @@ library(shiny)
 library(tidyverse)
 library(shinydashboard)
 
+# Import data
+raw_data = readr::read_tsv("Data/DATA2x02 survey (2022) - Form responses 1.tsv")
+
+# Clean data
+colnames(raw_data) = c("timestamp","covid_positive","living_arrangements","height","uni_travel_method","uni_travel_listen","spain_budget","feel_overseas","feel_anxious","study_hrs","read_news","study_load","work","lab_zoom","social_media","gender","sleep_time","wake_time","random_number","steak_preference","dominant_hand","normal_advanced","exercise_hrs","employment_hrs","city","weekly_saving","hourly_plan","weeks_behind","assignment_on_time","used_r_before","team_role","data2x02_hrs","social_media_hrs","uni_year","sport","wam","shoe_size","decade_selection")
+
+
+
+
 # Define UI for application that draws a histogram
 ui <- dashboardPage(title="DATA2X02 Survey Analysis",
                     # Application title
@@ -20,10 +29,7 @@ ui <- dashboardPage(title="DATA2X02 Survey Analysis",
                     dashboardSidebar(
                       sidebarMenu(
                         menuItem(text="Home", icon = icon("house"), tabName="home"),
-                        menuItem(text="Data Table", icon = icon("table"), tabName="dtable",
-                                 menuSubItem("Raw Data", tabName="rawdata"),
-                                 menuSubItem("Clean Data", tabName="cleandata")
-                                 ),
+                        menuItem(text="Data Table", icon = icon("table"), tabName="dtable"),
                         menuItem(text="Visualisations", icon = icon("image"), tabName="visualisations",
                                  menuSubItem("Histogram", tabName=("vis_hist")),
                                  menuSubItem("Scatter Plot", tabName=("vis_scat")),
@@ -38,6 +44,8 @@ ui <- dashboardPage(title="DATA2X02 Survey Analysis",
                       )),
                     
                     dashboardBody(
+                      
+                      # Styling Webpage ___________________________________________________________________________________
                       tags$style(HTML(".box-header {
                                       color:#fff;
                                       border-style:solid;
@@ -52,7 +60,12 @@ ui <- dashboardPage(title="DATA2X02 Survey Analysis",
                                       border-top-width:0px;
                                       }")
                                  ),
+                      
+                      
+                      # Tabs ___________________________________________________________________________________
                       tabItems(
+                        
+                        # Home Tab ___________________________________________________________________________________
                                     tabItem(tabName="home",
                                             box(width=12, title="Introduction to Data", solidHeader = TRUE,
                                                 htmlOutput("intro_text")),
@@ -61,17 +74,14 @@ ui <- dashboardPage(title="DATA2X02 Survey Analysis",
                                             box(width=12, title="Author", solidHeader = TRUE,
                                                 htmlOutput("author_text"))
                                     ),
-                                    tabItem(tabName="rawdata",
-                                            box(width=12, title="Introduction to Data"),
-                                            box(width=12, title="Packages & Versions"),
-                                            box(width=12, title="Author")
-                                    ),
-                                    tabItem(tabName="cleandata",
-                                            box(width=12,
-                                                column(width=6, "Hello World, it's cleandata!"),
-                                                column(width=6, "Test")
-                                            )
-                                    ),
+                                    
+                                    
+                        # Data Tables ___________________________________________________________________________________
+                        
+                                    tabItem(tabName="dtable", DT::dataTableOutput("raw_data", width="100%")),
+                        
+                        
+                        # Visualisations ___________________________________________________________________________________
                                     tabItem(tabName="vis_hist",
                                             box(width=12,
                                                 column(width=6, "Hello World, it's item2!"),
@@ -96,12 +106,18 @@ ui <- dashboardPage(title="DATA2X02 Survey Analysis",
                                                 column(width=6, "Test")
                                             )
                                     )
+                        
+                        
+                        # Hypothesis Tests ___________________________________________________________________________________
                     )
                     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  # Home Page ________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+  
   output$intro_text <- renderUI({
     HTML(paste("Welcome to the <b>DATA2X02 analysis shiny application</b>! Here,
                you will be able to quickly and easily analyse data from a survey
@@ -120,12 +136,34 @@ server <- function(input, output, session) {
                "<b>Tidyverse:</b>",
                "<b>Shiny:</b>",
                "<b>Shinydashboard:</b>",
+               "<b>DT:</b> used to create the interactive data tables. Find more information <a href='https://rstudio.github.io/DT/'>here</a>.",
                sep="<br><br>"))
   })
   output$author_text <- renderUI({
     HTML(paste("Made by <b>510456760</b> at the University of Sydney for
                DATA2902 in Semester 2, 2022.", sep="<br><br>"))
   })
+  
+  
+  
+  # Data Tables ________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+  
+  output$raw_data <- DT::renderDataTable(raw_data, options = list(scrollX = TRUE))
+  
+  
+  
+  
+  
+  
+  # Visualisations ________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+  
+  
+  
+  
+  
+  
+  # Hypothesis Tests ________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 }
 
 # Run the application 
